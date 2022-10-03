d3caf6bf9375a9edca5aa005876267638323d02a
Juan Gabriel Sousa Andrade <juangabriel4699@gmail.com>
2022-10-03 01:00:27 -0300

    Added react-native-maps

--- mobile/src/assets/screens/HomeScreen.tsx
+++ mobile/src/assets/screens/HomeScreen.tsx
@@ -1,18 +1,51 @@
-import { Dimensions, View } from 'react-native';
-import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import { useState } from 'react';
+import { View, Text, Dimensions, StyleSheet, Button } from 'react-native';
+import MapView, { Region } from 'react-native-maps';
 
 const HomeScreen = () => {
+  const [region, setRegion] = useState<Region>({
+    latitude: -15.7782081,
+    longitude: -47.93371,
+    latitudeDelta: 5,
+    longitudeDelta: 5,
+  });
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleMyLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+
+    const { latitude, longitude } = (await Location.getCurrentPositionAsync({})).coords;
+    setRegion({
+      latitude,
+      longitude,
+      latitudeDelta: 0.035,
+      longitudeDelta: 0.035,
+    });
+    setRegion((prev) => ({ ...prev, latitude, longitude }));
+  };
+
   return (
-    <View>
-      <MapView
-        style={{
-          width: Dimensions.get('window').width,
-          height: Dimensions.get('window').height,
-        }}
-        userInterfaceStyle="dark"
-      />
+    <View style={{ position: 'relative' }}>
+      {errorMsg && <Text>{errorMsg}</Text>}
+      <View style={{ position: 'absolute' }}>
+        <Button onPress={handleMyLocation} title="My location" color="#606060"/>
+      </View>
+      <MapView style={styles.map} region={region} showsUserLocation provider="google" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    position: 'absolute',
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height,
+  },
+});
 
 export default HomeScreen;
