0f57dc6318241e54242cd72e00b8f660e7d3d86b
Juan Andrade <juangabriel4699@gmail.com>
2022-10-18 22:19:07 -0300

    Fix MyLocation buttuon

--- mobile/src/screens/HomeScreen/ClusteredMapView.tsx
+++ mobile/src/screens/HomeScreen/ClusteredMapView.tsx
@@ -1,3 +1,4 @@
+import * as Location from 'expo-location';
 import { BBox, GeoJsonProperties } from 'geojson';
 import { useState, useRef } from 'react';
 import MapView, { Marker, Region } from 'react-native-maps';
@@ -6,19 +7,46 @@ import useSuperCluster from 'use-supercluster';
 
 import database from '../../database';
 import ClusteredMarker from './ClusteredMarker';
+import MyLocation from './MyLocation';
 import { regionToBoundingBox } from './utils';
 
 interface Props {
-  initialRegion: Region;
+  buttonMarginBottom: number;
 }
+
+const getMyLocation = async (): Promise<Region | undefined> => {
+  const { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== 'granted') return;
+
+  const { latitude, longitude } = (await Location.getCurrentPositionAsync({})).coords;
+  const region = {
+    latitude,
+    longitude,
+    latitudeDelta: 0.035,
+    longitudeDelta: 0.035,
+  };
+  return region;
+};
 
 const minMarkerSize = 30;
 
-const ClusteredMapView = ({ initialRegion }: Props) => {
-  const mapRef = useRef<MapView>(null);
+const ClusteredMapView = ({ buttonMarginBottom }: Props) => {
+  const initialRegion = {
+    latitude: -15.7782081,
+    longitude: -47.93371,
+    latitudeDelta: 5,
+    longitudeDelta: 5,
+  };
   const initialBound = regionToBoundingBox(initialRegion);
   const [bounds, setBounds] = useState<BBox>(initialBound);
   const [zoom, setZoom] = useState(10);
+
+  const mapRef = useRef<MapView>(null);
+
+  const setMyLocation = async () => {
+    const region = await getMyLocation();
+    region && mapRef.current?.animateToRegion(region, 1000);
+  };
 
   const onRegionChangeComplete = async (region: Region) => {
     const mapBound = regionToBoundingBox(region);
@@ -48,33 +76,36 @@ const ClusteredMapView = ({ initialRegion }: Props) => {
   });
 
   return (
-    <MapView
-      className="flex-1"
-      initialRegion={initialRegion}
-      provider="google"
-      ref={mapRef}
-      showsUserLocation
-      onRegionChangeComplete={onRegionChangeComplete}
-      testID="MapView">
-      {clusters?.map((point) => {
-        const [longitude, latitude] = point.geometry.coordinates;
-        const coordinate = { latitude, longitude };
-        const properties = point.properties ?? {};
+    <>
+      <MapView
+        className="flex-1"
+        testID="MapView"
+        provider="google"
+        initialRegion={initialRegion}
+        ref={mapRef}
+        onRegionChangeComplete={onRegionChangeComplete}
+        showsUserLocation>
+        {clusters?.map((point) => {
+          const [longitude, latitude] = point.geometry.coordinates;
+          const coordinate = { latitude, longitude };
+          const properties = point.properties ?? {};
 
-        if (properties?.cluster) {
-          const size = Math.max((properties.point_count * 40) / points.length, minMarkerSize);
-          return (
-            <ClusteredMarker
-              key={`cluster-${properties.cluster_id}`}
-              size={size}
-              coordinate={coordinate}
-              pointCount={properties.point_count}
-            />
-          );
-        }
-        return <Marker key={`marker-${properties.highId}`} coordinate={coordinate} />;
-      })}
-    </MapView>
+          if (properties?.cluster) {
+            const size = Math.max((properties.point_count * 40) / points.length, minMarkerSize);
+            return (
+              <ClusteredMarker
+                key={`cluster-${properties.cluster_id}`}
+                size={size}
+                coordinate={coordinate}
+                pointCount={properties.point_count}
+              />
+            );
+          }
+          return <Marker key={`marker-${properties.highId}`} coordinate={coordinate} />;
+        })}
+      </MapView>
+      <MyLocation mBottom={buttonMarginBottom} onPress={setMyLocation} />
+    </>
   );
 };
 
