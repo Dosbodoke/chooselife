c06e8e341610adbd2fb4711f439067a17f1d2173
Juan Andrade <juangabriel4699@gmail.com>
2023-01-05 16:26:38 -0300

    Change location of MapTypeScreen

--- mobile/src/screens/index.ts
+++ mobile/src/screens/index.ts
@@ -1,7 +1,7 @@
 import DetailScreen from './DetailScreen/DetailScreen';
 import HomeScreen from './Map/HomeScreen/HomeScreen';
 import LocationPickerScreen from './Map/LocationPickerScreen/LocationPickerScreen';
-import MapTypeScreen from './MapTypeScreen/MapTypeScreen';
+import MapTypeScreen from './Map/MapTypeScreen/MapTypeScreen';
 import SearchScreen from './SearchScreen/SearchScreen';
 
 export { HomeScreen, SearchScreen, MapTypeScreen, DetailScreen, LocationPickerScreen };
