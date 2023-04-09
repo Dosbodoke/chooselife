35298e8e045a5b2917ff84696f352c98376b5d17
Juan Andrade <juangabriel4699@gmail.com>
2023-04-09 16:00:58 -0300

    :sparkles: Added Apple oAuth

--- mobile/src/screens/SignIn/LogInWithApple.tsx
+++ mobile/src/screens/SignIn/LogInWithApple.tsx
@@ -1,12 +1,31 @@
+import { useCallback } from 'react';
 import { TouchableOpacity, Text } from 'react-native';
+import { useOAuth } from '@clerk/clerk-expo';
 
 import { AppleSvg } from '@src/assets';
 
 const LogInWithApple = () => {
+  const { startOAuthFlow } = useOAuth({ strategy: 'oauth_apple' });
+
+  const handleAppleSignIn = useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } = await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error('OAuth error', err);
+    }
+  }, []);
+
   return (
     <TouchableOpacity
       className="flex flex-row items-center justify-center rounded-md border-[1px] border-slate-300 py-3"
-      style={{ columnGap: 8 }}>
+      style={{ columnGap: 8 }}
+      onPress={handleAppleSignIn}>
       <AppleSvg />
       <Text className="text-base font-semibold">Entrar com Apple</Text>
     </TouchableOpacity>
