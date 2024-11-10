b212b7f1818503cb6a8e63fd9cbb61a7a985d7f1
Juan Andrade <juangabriel4699@gmail.com>
2024-11-10 18:29:22 -0300

    Highline dimensions card

--- components/highline/info.tsx
+++ components/highline/info.tsx
@@ -2,10 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import { useLocalSearchParams } from "expo-router";
 import { View } from "react-native";
 
-import { Text } from "~/components/ui/text";
 import { useAuth } from "~/context/auth";
 import { supabase } from "~/lib/supabase";
-import { H1, Lead } from "../ui/typography";
+
+import { Text } from "~/components/ui/text";
+import { H1, Lead } from "~/components/ui/typography";
+import { Card, CardContent } from "~/components/ui/card";
 
 export default function Info() {
   const { session } = useAuth();
@@ -25,19 +27,50 @@ export default function Info() {
   if (!highline) return null;
 
   return (
-    <View className="gap-2 flex-1">
+    <View className="flex-1 gap-4">
       <View>
         <H1>{highline.name}</H1>
         {highline.description ? <Lead>{highline.description}</Lead> : null}
       </View>
 
-      <InfoItem label={"Altura"} value={highline.height.toString()} />
-      <InfoItem label={"Comprimento"} value={highline.lenght.toString()} />
+      <HighlineDimensions height={highline.height} distance={highline.lenght} />
+
       <InfoItem label={"Fita principal"} value={highline.main_webbing} />
       <InfoItem label={"Fita backup"} value={highline.backup_webbing} />
     </View>
   );
 }
+
+const HighlineDimensions: React.FC<{
+  distance: number;
+  height: number;
+}> = ({ distance, height }) => (
+  <Card>
+    <CardContent className="flex flex-row justify-evenly items-center px-2 py-4 sm:gap-8">
+      <View className="flex items-center justify-center gap-2">
+        <View className="flex-row">
+          <Text className="text-3xl font-extrabold">{height}</Text>
+          <Text className="text-3xl font-extrabold text-muted-foreground">
+            m
+          </Text>
+        </View>
+        <Lead className="text-base">altura</Lead>
+      </View>
+
+      <View className="bg-gray-200 w-px h-full"></View>
+
+      <View className="flex items-center justify-center gap-2">
+        <View className="flex-row">
+          <Text className="text-3xl font-extrabold">{distance}</Text>
+          <Text className="text-3xl font-extrabold text-muted-foreground">
+            m
+          </Text>
+        </View>
+        <Lead className="text-base">comprimento</Lead>
+      </View>
+    </CardContent>
+  </Card>
+);
 
 const InfoItem = ({ label, value }: { label: string; value: string }) => {
   return (
