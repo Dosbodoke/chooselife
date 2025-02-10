d097eb1af8003749e72010d916b8abd3c44d8898
Juan Andrade <juangabriel4699@gmail.com>
2025-02-10 00:32:11 -0300

    Added minimum date back

--- app/highline/[id]/rig.tsx
+++ app/highline/[id]/rig.tsx
@@ -213,6 +213,7 @@ const DateForm: React.FC = () => {
                 mode="date"
                 locale="pt-BR"
                 date={value}
+                minimumDate={new Date()}
                 onDateChange={(date) => onChange(date)}
                 timeZoneOffsetInMinutes={0} // https://github.com/henninghall/react-native-date-picker/issues/841
                 theme={colorScheme.colorScheme}
