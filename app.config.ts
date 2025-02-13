0ec0ff3f8e478f2a70d78ae6af3f81cfe958ae21
Juan Andrade <juangabriel4699@gmail.com>
2025-02-13 10:03:56 -0300

    Bumped version to 1.1.0

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'ChooseLife',
   slug: 'chooselife',
-  version: '1.0.0',
+  version: '1.1.0',
   newArchEnabled: true,
   orientation: 'portrait',
   scheme: 'com.bodok.chooselife',
