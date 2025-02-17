d8cdc53dee1e7fbdfefb30d8d64c7cd4ea2bf2c8
Juan Andrade <juangabriel4699@gmail.com>
2025-02-17 09:05:33 -0300

    Fix offline banner layout shift and added children

--- components/offline-banner.tsx
+++ components/offline-banner.tsx
@@ -1,6 +1,4 @@
 import { useNetInfo } from '@react-native-community/netinfo';
-import { onlineManager } from '@tanstack/react-query';
-import { useAtomValue } from 'jotai';
 import { View } from 'react-native';
 import Animated, { FadeInUp, FadeOutUp } from 'react-native-reanimated';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -9,38 +7,49 @@ import { LucideIcon } from '~/lib/icons/lucide-icon';
 
 import { Text } from './ui/text';
 
-export const SafeAreaOfflineView: React.FC = () => {
+export const SafeAreaOfflineView: React.FC<{
+  children: React.ReactNode;
+  className?: string;
+}> = ({ children, className }) => {
   const { top } = useSafeAreaInsets();
   const { isConnected } = useNetInfo();
-  if (isConnected) return <View style={{ paddingTop: top }}></View>;
 
-  return (
-    <Animated.View
-      entering={FadeInUp}
-      exiting={FadeOutUp}
-      className="w-full py-2 bg-red-100 flex-row items-center justify-center gap-2"
-      style={{ paddingTop: top }}
-    >
-      <LucideIcon name="WifiOff" className="size-5 text-red-500" />
-      <Text className="text-red-500 flex-shrink">Você está offline</Text>
-    </Animated.View>
-  );
+  if (isConnected === false) {
+    return (
+      <Animated.View
+        entering={FadeInUp}
+        exiting={FadeOutUp}
+        style={{ paddingTop: top }}
+        className={className}
+      >
+        <View className="w-full py-2 bg-red-100 flex-row items-center justify-center gap-2">
+          <LucideIcon name="WifiOff" className="size-5 text-red-500" />
+          <Text className="text-red-500 flex-shrink">Você está offline</Text>
+        </View>
+        {children}
+      </Animated.View>
+    );
+  }
+
+  return <View style={{ paddingTop: top }}>{children}</View>;
 };
 
 export const OfflineBanner: React.FC = () => {
   const { top } = useSafeAreaInsets();
   const { isConnected } = useNetInfo();
-  if (isConnected) return null;
+  if (isConnected === false) {
+    return (
+      <Animated.View
+        entering={FadeInUp}
+        exiting={FadeOutUp}
+        className="w-full py-2 bg-red-100 flex-row items-center justify-center gap-2"
+        style={{ paddingTop: top }}
+      >
+        <LucideIcon name="WifiOff" className="size-5 text-red-500" />
+        <Text className="text-red-500 flex-shrink">Você está offline</Text>
+      </Animated.View>
+    );
+  }
 
-  return (
-    <Animated.View
-      entering={FadeInUp}
-      exiting={FadeOutUp}
-      className="w-full py-2 bg-red-100 flex-row items-center justify-center gap-2"
-      style={{ paddingTop: top }}
-    >
-      <LucideIcon name="WifiOff" className="size-5 text-red-500" />
-      <Text className="text-red-500 flex-shrink">Você está offline</Text>
-    </Animated.View>
-  );
+  return null;
 };
