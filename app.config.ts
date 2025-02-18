5c7f47bd550a4374ffc0fccf944c31cacde93bd0
Juan Andrade <juangabriel4699@gmail.com>
2025-02-18 00:03:39 -0300

    Updated code version for profile update feature

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'ChooseLife',
   slug: 'chooselife',
-  version: '1.2.2',
+  version: '1.3.0',
   newArchEnabled: true,
   orientation: 'portrait',
   scheme: 'com.bodok.chooselife',
