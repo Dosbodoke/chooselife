e1fa8b3582f24c39f8a902240fd40b48e9d8ff6e
Juan Andrade <juangabriel4699@gmail.com>
2025-03-13 00:29:31 -0300

    Removed google credential from remote

--- app.config.ts
+++ app.config.ts
@@ -1,79 +1,79 @@
-import { ConfigContext, ExpoConfig } from 'expo/config';
+import { ConfigContext, ExpoConfig } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: 'ChooseLife',
-  slug: 'chooselife',
-  version: '1.3.1',
+  name: "ChooseLife",
+  slug: "chooselife",
+  version: "1.3.1",
   newArchEnabled: true,
-  orientation: 'portrait',
-  scheme: 'com.bodok.chooselife',
+  orientation: "portrait",
+  scheme: "com.bodok.chooselife",
   userInterfaceStyle: "light",
-  assetBundlePatterns: ['**/*'],
+  assetBundlePatterns: ["**/*"],
   ios: {
     supportsTablet: true,
     icon: {
-      light: './assets/icons/ios-light.png',
-      dark: './assets/icons/ios-dark.png',
-      tinted: './assets/icons/ios-tinted.png',
-    }
+      light: "./assets/icons/ios-light.png",
+      dark: "./assets/icons/ios-dark.png",
+      tinted: "./assets/icons/ios-tinted.png",
+    },
   },
   android: {
-    softwareKeyboardLayoutMode: 'pan',
+    softwareKeyboardLayoutMode: "pan",
     adaptiveIcon: {
-      foregroundImage: './assets/icons/adaptive-icon.png',
-      monochromeImage: './assets/icons/adaptive-icon.png',
-      backgroundColor: '#ffffff',
+      foregroundImage: "./assets/icons/adaptive-icon.png",
+      monochromeImage: "./assets/icons/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
     permissions: [
-      'android.permission.ACCESS_COARSE_LOCATION',
-      'android.permission.ACCESS_FINE_LOCATION',
+      "android.permission.ACCESS_COARSE_LOCATION",
+      "android.permission.ACCESS_FINE_LOCATION",
     ],
-    package: 'com.bodok.chooselife',
-    googleServicesFile: "./google-services.json"
+    package: "com.bodok.chooselife",
+    googleServicesFile: "./credentials/google-services.json",
   },
   web: {
-    bundler: 'metro',
-    output: 'static',
-    favicon: './assets/icons/favicon.png',
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/icons/favicon.png",
   },
   plugins: [
     "expo-sqlite",
-    'expo-router',
-    'expo-localization',
+    "expo-router",
+    "expo-localization",
     [
       "expo-splash-screen",
       {
-        image: './assets/icons/splash-icon-dark.png',
+        image: "./assets/icons/splash-icon-dark.png",
         imageWidth: 200,
         resizeMode: "contain",
         backgroundColor: "#FFFFFF",
         dark: {
-          image: './assets/icons/splash-icon-light.png',
+          image: "./assets/icons/splash-icon-light.png",
           backgroundColor: "#000000",
-        }
-      }
+        },
+      },
     ],
     [
       "@rnmapbox/maps",
       {
-        "RNMapboxMapsDownloadToken": process.env.MAPBOX_DOWNLOAD_TOKEN 
-      }
-    ],
-    [
-      'expo-location',
-      {
-        locationAlwaysAndWhenInUsePermission:
-          'Allow $(PRODUCT_NAME) to use your location.',
+        "RNMapboxMapsDownloadToken": process.env.MAPBOX_DOWNLOAD_TOKEN,
       },
     ],
     [
-      'expo-asset',
+      "expo-location",
+      {
+        locationAlwaysAndWhenInUsePermission:
+          "Allow $(PRODUCT_NAME) to use your location.",
+      },
+    ],
+    [
+      "expo-asset",
       {
         assets: [
-          './assets/images/chooselife_black.png',
-          './assets/images/ui_dark.webp',
-          './assets/images/ui_light.webp',
+          "./assets/images/chooselife_black.png",
+          "./assets/images/ui_dark.webp",
+          "./assets/images/ui_light.webp",
         ],
       },
     ],
@@ -86,7 +86,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       origin: false,
     },
     eas: {
-      projectId: '6767d806-09e4-4095-ba1f-10805e3d0c5f',
+      projectId: "6767d806-09e4-4095-ba1f-10805e3d0c5f",
     },
   },
 });
