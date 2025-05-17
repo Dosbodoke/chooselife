20523322c90062b330e826559a7d742a194a4f5c
Juan Andrade <juangabriel4699@gmail.com>
2025-05-17 13:51:12 -0300

    Layout fixes

--- expo/app/highline/[id]/edit.tsx
+++ expo/app/highline/[id]/edit.tsx
@@ -1,17 +1,13 @@
-import { Stack, useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { ActivityIndicator, Platform, View } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ActivityIndicator, View } from 'react-native';
 
 import { useHighline } from '~/hooks/use-highline';
 
 import { HighlineForm } from '~/components/highline/highline-form';
 import { HighlineNotFound } from '~/components/highline/not-found';
-import { Text } from '~/components/ui/text';
 
 const EditHighlineScreen = () => {
-  const insets = useSafeAreaInsets();
   const { id } = useLocalSearchParams<{ id: string }>();
   const { highline, isPending } = useHighline({ id });
 
@@ -25,27 +21,7 @@ const EditHighlineScreen = () => {
 
   if (!highline) return <HighlineNotFound />;
 
-  return (
-    <>
-      <Stack.Screen
-        options={{
-          title: 'Edit Highline', // Optional title
-          headerBackVisible: true, // This enables the back button
-          headerBackTitle: 'Back',
-          // Optional custom back button:
-          headerLeft: (props) => <Text className="text-black">- Voltar</Text>,
-        }}
-      />
-      <KeyboardAwareScrollView
-        contentContainerStyle={{
-          paddingBottom: 32 + insets.bottom + insets.top, // pb-8 === 32px
-        }}
-        keyboardShouldPersistTaps="handled"
-      >
-        <HighlineForm highline={highline} />
-      </KeyboardAwareScrollView>
-    </>
-  );
+  return <HighlineForm highline={highline} />;
 };
 
 export default EditHighlineScreen;
