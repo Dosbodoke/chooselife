d18173db55c05ef1f083a887f12c6998b01bdcfe
Juan Andrade <juangabriel4699@gmail.com>
2025-06-06 14:37:26 -0300

    Save user name on profile creation trigger

--- supabase/migrations/20250605202359_handle_new_user_trigger.sql
+++ supabase/migrations/20250605202359_handle_new_user_trigger.sql
@@ -1,3 +1,16 @@
+create or replace function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+begin
+  insert into public.profiles (id, profile_picture, name)
+  values (new.id, new.raw_user_meta_data ->> 'avatar_url', new.raw_user_meta_data ->> 'full_name')
+  on conflict (id) do nothing; -- Prevents errors if the profile already exists
+  return new;
+end;
+$$;
+
 create trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user();
