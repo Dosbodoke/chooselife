433b74f67549a1b9057f40fd5e6c94dfa7ad57ff
Juan Andrade <juangabriel4699@gmail.com>
2025-06-09 09:56:44 -0300

    Bump version to publish a new IOS version

--- expo/app.config.ts
+++ expo/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "ChooseLife",
   slug: "chooselife",
-  version: "1.3.5",
+  version: "1.3.6",
   newArchEnabled: true,
   orientation: "portrait",
   scheme: "com.bodok.chooselife",
