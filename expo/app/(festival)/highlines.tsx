33f33db7d068b081581a71a7c0acb10f23602e7a
Juan Andrade <juangabriel4699@gmail.com>
2025-06-18 22:44:35 -0300

    Force brute festival highlines to be rigged

--- expo/app/(festival)/highlines.tsx
+++ expo/app/(festival)/highlines.tsx
@@ -8,9 +8,9 @@ import { HighlineMapCard } from '~/components/map/map-card';
 export default function HighlinesPage() {
   const { highlines } = useHighline({});
 
-  const festivalHighlines = highlines.filter((high) =>
-    highlinesID.includes(high.id),
-  );
+  const festivalHighlines = highlines
+    .filter((high) => highlinesID.includes(high.id))
+    .map((high) => ({ ...high, status: 'rigged' })); // Temporary solution because I have it with rig_date and is_rigged true on the DB and the status is still unrigged.
 
   return (
     <SafeAreaView className="flex-1 pt-6">
