f7c19a51a5d4f1e723cac17da61726d94355d49a
Juan Andrade <juangabriel4699@gmail.com>
2025-08-02 01:01:17 -0300

    New version '1.3.8'

--- expo/app.config.ts
+++ expo/app.config.ts
@@ -1,91 +1,91 @@
-import { ConfigContext, ExpoConfig } from 'expo/config';
+import { ConfigContext, ExpoConfig } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: 'ChooseLife',
-  slug: 'chooselife',
-  version: '1.3.7',
+  name: "ChooseLife",
+  slug: "chooselife",
+  version: "1.3.8",
   newArchEnabled: true,
-  orientation: 'portrait',
-  scheme: 'com.bodok.chooselife',
-  userInterfaceStyle: 'light',
-  assetBundlePatterns: ['**/*'],
+  orientation: "portrait",
+  scheme: "com.bodok.chooselife",
+  userInterfaceStyle: "light",
+  assetBundlePatterns: ["**/*"],
   ios: {
-    bundleIdentifier: 'com.bodok.chooselife',
+    bundleIdentifier: "com.bodok.chooselife",
     usesAppleSignIn: true,
     supportsTablet: false,
-    associatedDomains: ['applinks:chooselife.club'],
+    associatedDomains: ["applinks:chooselife.club"],
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
-      UIUserInterfaceStyle: 'Light',
+      UIUserInterfaceStyle: "Light",
     },
     config: {
       usesNonExemptEncryption: false,
     },
     icon: {
-      light: './assets/icons/ios-light.png',
-      dark: './assets/icons/ios-dark.png',
-      tinted: './assets/icons/ios-tinted.png',
+      light: "./assets/icons/ios-light.png",
+      dark: "./assets/icons/ios-dark.png",
+      tinted: "./assets/icons/ios-tinted.png",
     },
   },
   android: {
-    package: 'com.bodok.chooselife',
-    googleServicesFile: './credentials/google-services.json',
+    package: "com.bodok.chooselife",
+    googleServicesFile: "./credentials/google-services.json",
     intentFilters: [
       {
-        action: 'VIEW',
+        action: "VIEW",
         autoVerify: true,
         data: [
           {
-            scheme: 'https',
-            host: 'chooselife.club',
-            pathPattern: '.*highline/.*',
+            scheme: "https",
+            host: "chooselife.club",
+            pathPattern: ".*highline/.*",
           },
           {
-            scheme: 'https',
-            host: 'chooselife.club',
-            pathPattern: '.*profile/.*',
+            scheme: "https",
+            host: "chooselife.club",
+            pathPattern: ".*profile/.*",
           },
         ],
-        category: ['BROWSABLE', 'DEFAULT'],
+        category: ["BROWSABLE", "DEFAULT"],
       },
     ],
-    softwareKeyboardLayoutMode: 'pan',
+    softwareKeyboardLayoutMode: "pan",
     adaptiveIcon: {
-      foregroundImage: './assets/icons/adaptive-icon.png',
-      monochromeImage: './assets/icons/adaptive-icon.png',
-      backgroundColor: '#ffffff',
+      foregroundImage: "./assets/icons/adaptive-icon.png",
+      monochromeImage: "./assets/icons/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
     permissions: [
-      'android.permission.ACCESS_COARSE_LOCATION',
-      'android.permission.ACCESS_FINE_LOCATION',
+      "android.permission.ACCESS_COARSE_LOCATION",
+      "android.permission.ACCESS_FINE_LOCATION",
     ],
   },
   web: {
-    bundler: 'metro',
-    output: 'static',
-    favicon: './assets/icons/favicon.png',
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/icons/favicon.png",
   },
   plugins: [
-    'expo-apple-authentication',
-    'expo-sqlite',
-    'expo-router',
-    'expo-localization',
-    'expo-secure-store',
+    "expo-apple-authentication",
+    "expo-sqlite",
+    "expo-router",
+    "expo-localization",
+    "expo-secure-store",
     [
-      'expo-notifications',
+      "expo-notifications",
       {
-        icon: './assets/icons/android-notification-icon.png',
-        color: '#000000',
+        icon: "./assets/icons/android-notification-icon.png",
+        color: "#000000",
       },
     ],
     [
-      'expo-splash-screen',
+      "expo-splash-screen",
       {
-        image: './assets/icons/splash-icon-dark.png',
+        image: "./assets/icons/splash-icon-dark.png",
         imageWidth: 200,
-        resizeMode: 'contain',
-        backgroundColor: '#FFFFFF',
+        resizeMode: "contain",
+        backgroundColor: "#FFFFFF",
         // dark: {
         //   image: "./assets/icons/splash-icon-light.png",
         //   backgroundColor: "#000000",
@@ -93,24 +93,24 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       },
     ],
     [
-      '@rnmapbox/maps',
+      "@rnmapbox/maps",
       {
         RNMapboxMapsDownloadToken: process.env.MAPBOX_DOWNLOAD_TOKEN,
       },
     ],
     [
-      'expo-location',
+      "expo-location",
       {
         locationWhenInUsePermission:
-          '$(PRODUCT_NAME) uses your location to show nearby highlines and display your position on the map while exploring locations. Your location is not stored or shared.',
+          "$(PRODUCT_NAME) uses your location to show nearby highlines and display your position on the map while exploring locations. Your location is not stored or shared.",
       },
     ],
     [
-      'expo-asset',
+      "expo-asset",
       {
         assets: [
-          './assets/images/chooselife_black.png',
-          './assets/images/default_profile_picture.jpg',
+          "./assets/images/chooselife_black.png",
+          "./assets/images/default_profile_picture.jpg",
         ],
       },
     ],
@@ -123,13 +123,13 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       origin: false,
     },
     eas: {
-      projectId: '6767d806-09e4-4095-ba1f-10805e3d0c5f',
+      projectId: "6767d806-09e4-4095-ba1f-10805e3d0c5f",
     },
   },
   updates: {
-    url: 'https://u.expo.dev/6767d806-09e4-4095-ba1f-10805e3d0c5f',
+    url: "https://u.expo.dev/6767d806-09e4-4095-ba1f-10805e3d0c5f",
   },
   runtimeVersion: {
-    policy: 'appVersion',
+    policy: "appVersion",
   },
 });
