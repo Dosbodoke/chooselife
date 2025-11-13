0d53998338636b3557263078fd7115efa3c1519a
Juan Andrade <juangabriel4699@gmail.com>
2025-11-13 16:28:34 -0300

    Fix tabs title

--- expo/app/(tabs)/_layout.tsx
+++ expo/app/(tabs)/_layout.tsx
@@ -28,7 +28,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: t('app.(tabs)._layout.'),
+          title: t('app.(tabs)._layout.indexTitle'),
           tabBarHideOnKeyboard: true,
           tabBarIcon: ({ focused }) => (
             <LucideIcon
