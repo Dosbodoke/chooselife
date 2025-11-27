e0d6428ca945f0018ed1990c17da07d5a6fdc77c
Juan Andrade <juangabriel4699@gmail.com>
2025-11-27 11:35:52 -0300

    Adjust header info

--- expo/components/organizations/header-infos.tsx
+++ expo/components/organizations/header-infos.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { CalendarIcon, MapPinIcon, Users } from 'lucide-react-native';
+import { MapPinIcon, Users } from 'lucide-react-native';
 import React from 'react';
 import { View } from 'react-native';
 
@@ -32,7 +32,7 @@ export const HeaderInfos = ({ slug }: { slug: string }) => {
 
   return (
     <View>
-      <View className="flex-row items-center gap-6 my-4">
+      <View className="flex-row items-center gap-16 my-4">
         <View className="flex-row items-center gap-2">
           <View className="w-10 h-10 rounded-full bg-white/20 items-center justify-center">
             <Users className="text-black" size={20} />
@@ -51,21 +51,12 @@ export const HeaderInfos = ({ slug }: { slug: string }) => {
 
         <View className="flex-row items-center gap-2">
           <View className="w-10 h-10 rounded-full bg-white/20 items-center justify-center">
-            <CalendarIcon className="text-black" size={20} />
+            <MapPinIcon className="text-black" size={20} />
           </View>
           <View>
-            <Text className="text-black/70 text-xs font-semibold">Fundada</Text>
-            <Text className="text-black text-lg font-bold">2025</Text>
+            <Text className="text-black/70 text-xs font-semibold">Sede</Text>
+            <Text className="text-black text-lg font-bold">Anápolis</Text>
           </View>
-        </View>
-      </View>
-      <View className="flex-row items-center gap-2">
-        <View className="w-10 h-10 rounded-full bg-white/20 items-center justify-center">
-          <MapPinIcon className="text-black" size={20} />
-        </View>
-        <View>
-          <Text className="text-black/70 text-xs font-semibold">Sede</Text>
-          <Text className="text-black text-lg font-bold">Anápolis</Text>
         </View>
       </View>
     </View>
