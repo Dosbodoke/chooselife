a8f290f2b6afbbcd0f56e5fa564252bc6ceafdee
Juan Andrade <juangabriel4699@gmail.com>
2025-11-29 11:50:33 -0300

    membership feature flag

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,3 +3,4 @@ export * from './feature/organization/keys';
 export * from './feature/organization/use-organization';
 export * from './feature/organization/use-is-member';
 export * from './feature/organization/use-news';
+export * from './feature/flags';
