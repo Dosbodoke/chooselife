33dff950c98546aee3beb2e308444760cffbd522
Juan Andrade <juangabriel4699@gmail.com>
2026-03-23 10:55:05 -0300

    Displat user profile picture for profile tab icon

--- expo/app/(tabs)/_layout.tsx
+++ expo/app/(tabs)/_layout.tsx
@@ -5,14 +5,18 @@ import {
   UserCircleIcon,
   UsersRoundIcon,
 } from 'lucide-react-native';
+import { View } from 'react-native';
 import { useTranslation } from 'react-i18next';
 
+import { useAuth } from '~/context/auth';
 import { cn } from '~/lib/utils';
 
+import { SupabaseAvatar } from '~/components/supabase-avatar';
 import { Icon } from '~/components/ui/icon';
 
 export default function TabLayout() {
   const { t } = useTranslation();
+  const { profile } = useAuth();
   return (
     <Tabs>
       <Tabs.Screen
@@ -71,15 +75,25 @@ export default function TabLayout() {
         options={{
           title: t('app.(tabs)._layout.settingsTitle'),
           headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <Icon
-              as={UserCircleIcon}
-              className={cn(
-                'size-6',
-                focused ? 'text-blue-500' : 'text-muted-foreground',
-              )}
-            />
-          ),
+          tabBarIcon: ({ focused }) =>
+            profile ? (
+              <View
+                className={cn(
+                  'size-7 rounded-full',
+                  focused && 'border-2 border-blue-500',
+                )}
+              >
+                <SupabaseAvatar profileID={profile.id} />
+              </View>
+            ) : (
+              <Icon
+                as={UserCircleIcon}
+                className={cn(
+                  'size-6',
+                  focused ? 'text-blue-500' : 'text-muted-foreground',
+                )}
+              />
+            ),
         }}
       />
     </Tabs>
