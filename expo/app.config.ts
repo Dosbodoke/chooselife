ec559e0e7ce6030f962a626d2a2bdaf857fc643f
Juan Andrade <juangabriel4699@gmail.com>
2026-03-26 14:12:19 -0300

    Bump version to 1.4.0

--- expo/app.config.ts
+++ expo/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'ChooseLife',
   slug: 'chooselife',
-  version: '1.3.18',
+  version: '1.4.0',
   experiments: {
     typedRoutes: true,
     reactCompiler: true,
