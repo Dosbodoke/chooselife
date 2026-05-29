7ac2dd666dd6fc71173799a11e6b85e631e652db
Juan Andrade <juangabriel4699@gmail.com>
2026-05-28 23:23:16 -0300

    1.6.3

--- expo/app.config.ts
+++ expo/app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'ChooseLife',
   slug: 'chooselife',
-  version: '1.6.2',
+  version: '1.6.3',
   experiments: {
     typedRoutes: true,
     reactCompiler: true,
