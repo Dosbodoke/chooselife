54aeb05222e7a77983c2acc77e38cab20a29fbd7
Juan Andrade <juangabriel4699@gmail.com>
2026-09-16 01:43:46 -0300

    chore: bump app version to 1.8.6

--- expo/app.config.ts
+++ expo/app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "ChooseLife",
   slug: "chooselife",
-  version: "1.8.5",
+  version: "1.8.6",
   experiments: {
     typedRoutes: true,
     reactCompiler: true,
